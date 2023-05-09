6b6ab80cf2105138d504d2f5811b1ae9a8ca9f17
Diego Casermeiro Gálvez <122561493+diegokser@users.noreply.github.com>
2023-05-09 16:58:34 +0000

    cambios en archivos app index y style

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let stick = ["♦", "♥", "♠", "♣"];
+  let stickRandom = stick[Math.floor(Math.random() * stick.length)];
+  let numbersRandom = numbers[Math.floor(Math.random() * numbers.length)];
+
+  document.querySelector("#palo-top").innerHTML = stickRandom;
+  document.querySelector("#palo-bottom").innerHTML = stickRandom;
+
+  if (stick[stickRandom] == "♦" || stick) {
+    let rombo = document.querySelector("#palo-bottom");
+    rombo.style.color = "red";
+  } else if (stick[stickRandom] == ":corazones:") {
+    let heart = document.querySelector("#palo-bottom");
+    heart.style.color = "red";
+  }
+
+  document.querySelector("p").innerHTML = numbersRandom;
 };
